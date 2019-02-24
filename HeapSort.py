47a33047b3e71bab401212d35b0ddae7fa50730c
zyabxwcd <zyabxwcd@gmail.com>
2019-02-24 21:08:22 +0530

    added output of the code

--- HeapSort.py
+++ HeapSort.py
@@ -1,40 +1,47 @@
+'''
+Output of the code below:
+4 -1 1 0 0 8 32
+Input number list: [4, -1, 1, 0, 0, 8, 32]
+After heap sort: [-1, 0, 0, 1, 4, 8, 32]
+'''
+
 # Function to maintain max-heap property
 def max_heapify(data, n, i):
     largest = i
-    l = 2 * i + 1 # Left child position
-    r = 2 * i + 2 # Right child position
- 
+    l = 2 * i + 1  # Left child position
+    r = 2 * i + 2  # Right child position
+
     # Compare left child and root
     if l < n and data[i] < data[l]:
         largest = l
- 
+
     # Compare right child and root
     if r < n and data[largest] < data[r]:
         largest = r
- 
+
     # Change root, if needed, that is if it’s not the largest
     if largest != i:
         # Swap current root and largest
-        data[i],data[largest] = data[largest],data[i]
- 
+        data[i], data[largest] = data[largest], data[i]
+
         # Heapify the root
         max_heapify(data, n, largest)
- 
+
 
 # The main function to sort an array
 def heap_sort(data):
     n = len(data)
- 
+
     # Building a maxheap
     for i in range(n, -1, -1):
         max_heapify(data, n, i)
- 
+
     # Extracting elements one at a time
     for i in range(n-1, 0, -1):
         data[i], data[0] = data[0], data[i]
         max_heapify(data, i, 0)
-  
-  
+
+
 num = [int(i) for i in input().split()]
 print('Input number list:', num)
 
